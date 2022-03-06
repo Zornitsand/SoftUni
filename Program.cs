00bb254440631d648e7124629909af4c1ae0e875
Zornitsand <100421029+Zornitsand@users.noreply.github.com>
2022-03-06 21:36:19 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] elements = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] numbersToBePushed = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> basicStack = new Stack<int>(numbersToBePushed);
+
+            int numbersToPop = elements[1];
+            int numberToLookFor = elements[2];
+            for (int i = 0; i < numbersToPop; i++)
+            {
+                basicStack.Pop();
+            }
+            if (basicStack.Contains(numberToLookFor))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                if (basicStack.Count > 0)
+                {
+                    Console.WriteLine(basicStack.Min());
+                }
+                else
+                {
+                    Console.WriteLine(basicStack.Count);
+                }
+            }
+        }
+    }
+}
